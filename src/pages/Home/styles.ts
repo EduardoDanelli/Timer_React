273ab029b9f82fe4fae686d4bad7dbbafad06a26
EduardoDanelli <eduardodanelli@gmail.com>
2024-07-15 10:08:15 -0300

    initial commit

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+    height: 100%;
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 3.5rem;
+    }
+`
+export const FormContainer = styled.div``
+
+export const CountDownContainer = styled.div``
